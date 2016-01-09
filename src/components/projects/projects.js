1130b2549e94592bbfae151c55706c494fc4fdeb
davidbastian <d@davidbastian.cl>
2016-01-09 22:00:30 +1300

    updated projects mainfeild

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import './projects.scss'
 
 import p from 'json!./projects.json'
@@ -9,7 +8,14 @@ const pjson = p.projects
 
 export default class projects extends React.Component {
 
+
+  constructor(props) {
+        super(props)
+        this.state = { empleados: [] }
+  }
+
   componentWillMount() {  
+
    // console.log('hola');
      const slug = this.props.params.slug
       console.log(slug)
@@ -22,14 +28,10 @@ export default class projects extends React.Component {
   /*console.log(slug)*/
 
 	var newProject = [];
-
 	var selectedUser = pjson.find( function(project){
-
-      const getslug = project.name.toLowerCase().split(' ').join('-');
+  const getslug = project.name.toLowerCase().split(' ').join('-');
 
       /*console.log(per)*/ 
-
-
 
 	  if (slug === getslug) {		
                 newProject.push(project);
@@ -49,22 +51,23 @@ export default class projects extends React.Component {
 
           <div className="descriptionInner">
                <h2 dangerouslySetInnerHTML={{__html: newProject[0].title}} /> 
-               <p>{newProject[0].description}</p>
+                <p dangerouslySetInnerHTML={{__html: newProject[0].description}} /> 
+
            </div>       
         
         </div> 
         <div className="images">
 
-                <img src={newProject[0].img} alt={newProject[0].description}/>
+  
+                    
                     {
                           
                               newProject[0].media.map(
 
-                            a =>  <img key={a.img} src={a.img} alt={a.img}/>     
+                                  a =>  <img key={a.img} src={a.img} alt={a.img}/>     
                              
-                          )
+                              )
                     } 
-
         </div>
         
  		
