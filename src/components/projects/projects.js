28464d344ff80935d4abdee7bc145b910379d8d8
David Bastian <d@davidbastian.cl>
2016-01-15 18:48:34 +1300

    a

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -239,7 +239,16 @@ export default class projects extends React.Component {
 
           <div className="descriptionInner">
                 <h2 key={'h2'} dangerouslySetInnerHTML={{__html: newProject[0].title}} /> 
-                <p  key={'p'} dangerouslySetInnerHTML={{__html: newProject[0].description}} /> 
+                
+
+                <div className="credits">
+                    <p  key={'p'} dangerouslySetInnerHTML={{__html: newProject[0].description}} /> 
+                      <ul>
+                            <li>Agency: Designworks</li>
+                            <li>Year: 2015</li>
+                            <li><a href="#"><span className="see">See Demo </span><span className="plane">✈</span></a></li>
+                      </ul>
+                </div>
            </div>       
         
         </div> 
