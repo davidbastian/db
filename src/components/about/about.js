2e1b037061552ded760506b0d2e0e90c0ee3a598
davidbastian <d@davidbastian.cl>
2016-01-19 02:06:24 +1300

    LAST

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -12,12 +12,8 @@ constructor(props) {
   render() {
     return (
 	 		<div className="container about">
-					<h2>I am a Designer & Web Developer from Viña del Mar (Chile), based in New Zealand. I love to enjoy good art, Conceptualising and Designing simple and clean interfaces.</h2>
-
-<h2>I work through multiples design disciplines, such as Interactive, Illustration, Web Development, Art Direction & Photography. For each project always I choose the one that have the major impact. 
-I believe in the value of a good idea and the trends changes.</h2>
-<h2>It is not enough look the web as a piece of design, Also need its comprehension and a deep knowledge of the Social & Cultural Environment, History Art, Composition & Color Theory.</h2>   
-  </div>)
+					<h2>I am a Designer & Web Developer from Viña del Mar (Chile), based in New Zealand. I love to enjoy and create Good Art, Conceptualising and Designing simple and clean interfaces.</h2>
+ </div>)
 
 
 }
